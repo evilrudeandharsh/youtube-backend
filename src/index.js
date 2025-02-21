c101725312ec85487e4058a0651800d625d4a0f9
Aadi Vinayak <aadivinayak36@gmail.com>
2025-02-21 11:21:34 +0530

    add user and video models, configure CORS, and implement API response/error handling

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+
+        console.log(`Server running at port : ${process.env.PORT}`)
+    })
+
+
+
+    
+})
+.catch((err)=>{console.log("MONGODB CONNECTION FAILED")})
 
 
 
