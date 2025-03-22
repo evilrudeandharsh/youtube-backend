b210fc79d6c85ebaee9f6a297e303872ec8d48cb
Aadi Vinayak <aadivinayak36@gmail.com>
2025-03-22 08:44:51 +0530

    feat: implement video retrieval, update, and deletion functionality; add error handling for invalid video IDs

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -15,27 +15,63 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
+    
     // TODO: get video, upload to cloudinary, create video
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    const videos=await Video.findById(videoId)
+    if(!videos){
+        throw new ApiError(404, "Video not found")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200,videos,"Video fetched successfully"))
     //TODO: get video by id
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    const { title, description, thumbnail } = req.body
+    const video=await Video.findByIdAndUpdate(videoId,
+        {
+            $set:{
+                title,
+                description,
+                thumbnail
+            }
+
+        },
+        {new:true}
+    )
+return res
+.status(200)
+.json(new ApiResponse(200, video, "Video details updated successfully"))
     //TODO: update video details like title, description, thumbnail
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video id")
+    }
+    const video = await Video.findByIdAndDelete(videoId)
+    return res
+    .status(200)
+    .json(new ApiResponse(200,video,"Video deleted succesfully"))
+
+    
+
     //TODO: delete video
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    const video = await Video.findById(videoId)
+    
+
 })
 
 export {
