4ef9a853e292bd303be98416b69d319e0264cc1f
Aadi Vinayak <aadivinayak36@gmail.com>
2025-03-22 14:55:16 +0530

    feat: enhance video retrieval and publishing functionality; add pagination, search, and error handling

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,12 +9,60 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    
+    const user=await User.findById(userId)
+
+    const pageNo=parseInt(page)
+    const limitNo=parseInt(limit)
+
+    searchQuery={}
+    if(query){
+        searchQuery.title={$regex:query,$options:"i"}
+                   .description={$regex:query,$options:"i"}
+
+    }
+    const videos=await Video.find(searchQuery)
+    .populate("owner")
+    .sort(sortType === "desc" ? {[sortBy]:-1}:{[sortBy]:1})
+    .limit(limitNo)
+
+    const totalVideos = await Video.countDocuments(searchConditions);
+
+    const totalPages = Math.celi(totalVideos / pageLimit);
+
+    return res.status(200).json(
+        new ApiResponse({
+        success: true,
+        message: "Videos retrived successfully",
+        data: videos,
+        pagination: {
+            page: pageNumber,
+            limit: pageLimit,
+            totalPages,
+            totalVideos,
+        },
+        }))
     //TODO: get all videos based on query, sort, pagination
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
+    const localpathvideo=req.files?.videoFile[0]?.path
+    const localpaththumbnail=req.files?.thumbnail[0]?.path
+    const videoFile=await uploadOnCloudinary(localpathvideo)
+    const thumbNail=await uploadOnCloudinary(localpaththumbnail)
+
+    const video=await Video.create({
+        videoFile,
+        title,
+        description,
+        thumbNail,
+        owner:req.user._id
+    })
+    return res
+    .status(201)
+    .json(new ApiResponse(201,video,"Video published successfully"))
+
+
     
     // TODO: get video, upload to cloudinary, create video
 })
@@ -70,7 +118,10 @@ const deleteVideo = asyncHandler(async (req, res) => {
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     const video = await Video.findById(videoId)
-    
+    if(!video){
+        throw new ApiError(404, "Video not found")
+    }
+
 
 })
 
